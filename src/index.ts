3ca32f186872b0cbe336aff68091fd7b289514fe
mircoocrim98 <mirco.lange@outlook.com>
2024-08-08 23:33:41 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,21 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get("/login", (req: Request, res: Response) => {
-  res.send("Hello, world!");
+app.post("/login", (req: Request, res: Response) => {
+
 });
 
-app.post("/data", (req: Request, res: Response) => {
+app.post("/register", (req: Request, res: Response) => {
   const data = req.body;
-  res.json({ received: data });
+})
+
+app.post("/setHighscore", (req: Request, res: Response) => {
+
 });
+
+app.get("/getHighscore", (req: Request, res: Response) => {
+
+})
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
