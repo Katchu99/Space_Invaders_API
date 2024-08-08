66e8529460bf3a02d54ea6def676052416c0294c
Katchu99 <Maariius@outlook.de>
2024-08-08 22:49:25 +0200

    File structure changes

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/login", (req: Request, res: Response) => {
   res.send("Hello, world!");
 });
 
