41e6586fde7c3200e8c3128310786740eaccbe8d
Katchu99 <Maariius@outlook.de>
2024-08-08 22:28:09 +0200

    API-Beispiel eingefügt

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, world!");
+});
+
+app.post("/data", (req: Request, res: Response) => {
+  const data = req.body;
+  res.json({ received: data });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
