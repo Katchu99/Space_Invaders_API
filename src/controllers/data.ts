e190c3fe8935252824025f5167d0808d047a755d
Katchu99 <Maariius@outlook.de>
2024-08-12 02:07:09 +0200

    added /highscores/set functionality

--- src/controllers/data.ts
+++ src/controllers/data.ts
@@ -1,8 +1,31 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { DataModel } from "../models/data";
+import { CustomRequest } from "../types/types";
 
 export class DataController {
   constructor(private model: DataModel) {}
 
-  async setHighscore(req: Request, res: Response) {}
+  async setHighscore(req: CustomRequest, res: Response) {
+    try {
+      const score = req.body;
+      const userId = req.userId;
+
+      if (!userId || !score) {
+        return res.status(400).json({ error: "Invalid request" }).end();
+      }
+      const result = await this.model.setHighscore(userId, score);
+
+      if (result.success) {
+        res.status(200).end();
+      } else {
+        res.status(400).json({ error: result.error }).end();
+      }
+    } catch (err) {
+      console.log(`Error setting highscore: ${err}`);
+      res
+        .status(500)
+        .json({ error: `Failed to set highscore: ${err}` })
+        .end();
+    }
+  }
 }
--- src/models/data.ts
+++ src/models/data.ts
@@ -2,4 +2,44 @@ import { Database } from "sqlite";
 
 export class DataModel {
   constructor(private db: Database) {}
+
+  async setHighscore(userId: string, highscore: number[]) {
+    try {
+      if (!Array.isArray(highscore)) {
+        throw new Error("Highscore must be an array");
+      }
+
+      const existingHighscores = await this.db.get(
+        "SELECT highscore FROM personalHighscore WHERE user_id = ?",
+        userId
+      );
+
+      if (existingHighscores) {
+        const existingHighscoresArray = JSON.parse(
+          existingHighscores.highscore
+        );
+
+        // Merge the two arrays and remove duplicates
+        const mergedHighscores = [
+          ...new Set([...existingHighscoresArray, ...highscore]),
+        ];
+
+        await this.db.run(
+          "UPDATE personalHighscore SET highscore=? WHERE user_id=?",
+          JSON.stringify(mergedHighscores),
+          userId
+        );
+      } else {
+        await this.db.run(
+          "INSERT INTO personalHighscore (user_id, highscore) VALUES (?, ?)",
+          userId,
+          JSON.stringify(highscore)
+        );
+      }
+
+      return { success: true };
+    } catch (err) {
+      return { success: false, error: err };
+    }
+  }
 }
