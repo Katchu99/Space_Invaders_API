fc8a1c15de24f045bf7343b60361767fbccd48e7
Katchu99 <Maariius@outlook.de>
2024-08-12 02:06:03 +0200

    added /highscores/set route

--- src/routes/highscores.ts
+++ src/routes/highscores.ts
@@ -1,12 +1,17 @@
 import express, { Response } from "express";
 import { CustomRequest } from "../types/types";
 import { verifyToken } from "../middleware/verifyToken";
+import { DataModel } from "../models/data";
+import { DataController } from "../controllers/data";
 
 export default (router: express.Router, db: any) => {
+  const dataModel = new DataModel(db);
+  const dataController = new DataController(dataModel);
+
   router.get("/highscores/get", (req: CustomRequest, res: Response) => {});
   router.post(
     "/highscores/set",
     verifyToken,
-    (req: CustomRequest, res: Response) => {}
+    (req: CustomRequest, res: Response) => dataController.setHighscore(req, res)
   );
 };
